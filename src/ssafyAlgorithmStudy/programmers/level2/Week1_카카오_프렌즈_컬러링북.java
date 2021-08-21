24d38dbe9e323ecab40798558259e867e7cbeb81
juhwankim-dev <76620764+juhwankim-dev@users.noreply.github.com>
2021-08-21 21:09:36 +0900

    정렬이 이상해서 카카오 프렌즈 컬러링북 다시 업로드

--- src/ssafyAlgorithmStudy/programmers/level2/Week1_카카오_프렌즈_컬러링북.java
+++ src/ssafyAlgorithmStudy/programmers/level2/Week1_카카오_프렌즈_컬러링북.java
@@ -9,18 +9,18 @@ public class Week1_카카오_프렌즈_컬러링북 {
 	static int maxArea; // 가장 큰 영역이 몇칸인지
 	static int limitY;
 	static int limitX;
-	
+
 	public static void main(String[] args) {
 
 	}
 
 	public int[] solution(int m, int n, int[][] picture) {
 		visited = new boolean[m][n];
-        cntArea = 0;
+		cntArea = 0;
 		maxArea = 0;
 		limitY = m;
 		limitX = n;
-		
+
 		for (int y = 0; y < m; y++) {
 			for (int x = 0; x < n; x++) {
 				if (!visited[y][x] && picture[y][x] != 0) {
@@ -31,28 +31,28 @@ public class Week1_카카오_프렌즈_컬러링북 {
 				}
 			}
 		}
-		
+
 		int[] answer = { cntArea, maxArea };
 		return answer;
-    }
-    
+	}
+
 	public static void dfs(int[][] picture, int y, int x) {
 		visited[y][x] = true;
 		cnt++;
 		int color = picture[y][x];
-		
+
 		for (int i = 0; i < 4; i++) {
 			int newY = y + dy[i];
 			int newX = x + dx[i];
-			
-			if(!outOfIndex(newY, newX) && !visited[newY][newX] && picture[newY][newX] == color) {
+
+			if (!outOfIndex(newY, newX) && !visited[newY][newX] && picture[newY][newX] == color) {
 				dfs(picture, newY, newX);
 			}
 		}
 	}
 
 	public static boolean outOfIndex(int y, int x) {
-		if(y < 0 || x < 0 || y >= limitY || x >= limitX) {
+		if (y < 0 || x < 0 || y >= limitY || x >= limitX) {
 			return true;
 		}
 		return false;
