5e56255140f9a6c1b0f50b4cb05a2a33d56c960c
juhwankim-dev <76620764+juhwankim-dev@users.noreply.github.com>
2021-08-21 21:03:41 +0900

    124 나라의 숫자

--- src/ssafyAlgorithmStudy/programmers/level2/Week1_124_나라의_숫자.java
+++ src/ssafyAlgorithmStudy/programmers/level2/Week1_124_나라의_숫자.java
@@ -3,8 +3,18 @@ package ssafyAlgorithmStudy.programmers.level2;
 public class Week1_124_나라의_숫자 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+    public String solution(int n) {
+    	String[] num = {"4", "1", "2"};
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0) {
+        	sb.append(num[n % 3]);
+        	n = (n - 1) / 3;
+        }
+
+        return sb.reverse().toString();
+    }
 }
